06b408e0938cc86b594e5e44429ac4a9b51fcf44
Luciano Raguzzoni <62899636+lumreng@users.noreply.github.com>
2020-07-07 14:33:13 -0300

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ const { Pool } = require("pg");
 
 module.exports = new Pool({
     user: "postgres",
-    password: "lumera78",
+    password: "...",
     host: "localhost",
     port: 5432,
     database: "launchstoredb"
